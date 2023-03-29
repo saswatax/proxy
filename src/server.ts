e36776521268280fcf0992dca0817968b58d8cd6
Saswata <saswata.bhunia@venturepact.com>
2023-03-29 10:41:58 +0530

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import cors from "cors";
 
-const PORT = process.env.PORT;
-const REDIRECT_URL = process.env.REDIRECT_URL;
+const PORT = process.env.PORT || 5000;
+const REDIRECT_URL = "https://reqres.in";
 const app = express();
 
 app.use(morgan("dev"));
+
+app.use(cors());
+app.options("/", cors());
 
 app.use("/status", (req, res) => res.send("running"));
 app.use("/", createProxyMiddleware({ target: REDIRECT_URL, changeOrigin: true }));
