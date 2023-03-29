5bb08ffb4c2095140b2f2437aa70c6f886c3d225
Saswata <saswata.bhunia@venturepact.com>
2023-03-29 09:39:06 +0530

    fix

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import express from "express";
 import morgan from "morgan";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+const REDIRECT_URL = process.env.REDIRECT_URL;
 const app = express();
-
+console.log(PORT, REDIRECT_URL)
 app.use(morgan("dev"));
 
 app.use("/status", (req, res) => res.send("running"));
-app.use("/", createProxyMiddleware({ target: "http://localhost:4000", changeOrigin: true }));
+app.use("/", createProxyMiddleware({ target: REDIRECT_URL, changeOrigin: true }));
 
 app.listen(PORT, () => console.log(`Proxy running PORT: ${PORT}`));
